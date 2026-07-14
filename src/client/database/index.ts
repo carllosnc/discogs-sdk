e3b47db91696ce85379c4700863e93aa5e833ba4
Carlos Costa <carllos.nc@gmail.com>
2026-07-13 21:42:26 -0300

    feat(database): add database client

---
+++ src/client/database/index.ts
@@ -0,0 +1,64 @@
+﻿import { type HttpClient } from "../../http/types.js";
+import {
+  type DiscogsArtist,
+  type DiscogsLabel,
+  type DiscogsMaster,
+  type DiscogsRelease,
+  type DiscogsSearchParams,
+  type DiscogsSearchResponse,
+  type DiscogsVersionsParams,
+  type DiscogsVersionsResponse,
+} from "../../types/database.js";
+import { buildQueryParams } from "../../utils/query.js";
+
+export class DatabaseClient {
+  constructor(private httpClient: HttpClient) {}
+
+  async search(params: DiscogsSearchParams): Promise<DiscogsSearchResponse> {
+    const response = await this.httpClient.get<DiscogsSearchResponse>("database/search", {
+      params: buildQueryParams(params),
+    });
+    return response.data;
+  }
+
+  async getRelease(releaseId: number): Promise<DiscogsRelease> {
+    const response = await this.httpClient.get<DiscogsRelease>(`releases/${releaseId}`);
+    return response.data;
+  }
+
+  async getMaster(masterId: number): Promise<DiscogsMaster> {
+    const response = await this.httpClient.get<DiscogsMaster>(`masters/${masterId}`);
+    return response.data;
+  }
+
+  async getMasterVersions(masterId: number, params?: DiscogsVersionsParams): Promise<DiscogsVersionsResponse> {
+    const response = await this.httpClient.get<DiscogsVersionsResponse>(`masters/${masterId}/versions`, {
+      params: buildQueryParams(params),
+    });
+    return response.data;
+  }
+
+  async getArtist(artistId: number): Promise<DiscogsArtist> {
+    const response = await this.httpClient.get<DiscogsArtist>(`artists/${artistId}`);
+    return response.data;
+  }
+
+  async getArtistReleases(artistId: number, params?: DiscogsVersionsParams): Promise<DiscogsVersionsResponse> {
+    const response = await this.httpClient.get<DiscogsVersionsResponse>(`artists/${artistId}/releases`, {
+      params: buildQueryParams(params),
+    });
+    return response.data;
+  }
+
+  async getLabel(labelId: number): Promise<DiscogsLabel> {
+    const response = await this.httpClient.get<DiscogsLabel>(`labels/${labelId}`);
+    return response.data;
+  }
+
+  async getLabelReleases(labelId: number, params?: DiscogsVersionsParams): Promise<DiscogsVersionsResponse> {
+    const response = await this.httpClient.get<DiscogsVersionsResponse>(`labels/${labelId}/releases`, {
+      params: buildQueryParams(params),
+    });
+    return response.data;
+  }
+}
---
+++ src/types/database.ts
@@ -0,0 +1,173 @@
+﻿import { type DiscogsPaginatedResponse, type DiscogsPaginationParams } from "./common.js";
+
+export type DiscogsSearchType = "release" | "master" | "artist" | "label";
+
+export interface DiscogsSearchParams extends DiscogsPaginationParams {
+  query?: string;
+  type?: DiscogsSearchType;
+  title?: string;
+  releaseTitle?: string;
+  credit?: string;
+  artist?: string;
+  anv?: string;
+  label?: string;
+  genre?: string;
+  style?: string;
+  country?: string;
+  year?: string | number;
+  format?: string;
+  catno?: string;
+  barcode?: string;
+  track?: string;
+  submitter?: string;
+  contributor?: string;
+}
+
+export interface DiscogsSearchResult {
+  id: number;
+  type: DiscogsSearchType;
+  title: string;
+  uri?: string;
+  resource_url: string;
+  thumb?: string;
+  cover_image?: string;
+  master_id?: number;
+  master_url?: string;
+  country?: string;
+  year?: string;
+  format?: string[];
+  label?: string[];
+  genre?: string[];
+  style?: string[];
+  catno?: string;
+  barcode?: string[];
+}
+
+export type DiscogsSearchResponse = DiscogsPaginatedResponse<DiscogsSearchResult>;
+
+export interface DiscogsImage {
+  type: "primary" | "secondary" | string;
+  uri: string;
+  resource_url: string;
+  uri150?: string;
+  width?: number;
+  height?: number;
+}
+
+export interface DiscogsArtistSummary {
+  id?: number;
+  name: string;
+  anv?: string;
+  join?: string;
+  role?: string;
+  tracks?: string;
+  resource_url?: string;
+}
+
+export interface DiscogsTrack {
+  position: string;
+  type_: string;
+  title: string;
+  duration?: string;
+  extraartists?: DiscogsArtistSummary[];
+}
+
+export interface DiscogsRelease {
+  id: number;
+  title: string;
+  artists: DiscogsArtistSummary[];
+  data_quality?: string;
+  thumb?: string;
+  community?: unknown;
+  companies?: unknown[];
+  country?: string;
+  date_added?: string;
+  date_changed?: string;
+  estimated_weight?: number;
+  extraartists?: DiscogsArtistSummary[];
+  format_quantity?: number;
+  formats?: unknown[];
+  genres?: string[];
+  identifiers?: unknown[];
+  images?: DiscogsImage[];
+  labels?: unknown[];
+  lowest_price?: number;
+  master_id?: number;
+  master_url?: string;
+  notes?: string;
+  num_for_sale?: number;
+  released?: string;
+  released_formatted?: string;
+  resource_url: string;
+  series?: unknown[];
+  status?: string;
+  styles?: string[];
+  tracklist?: DiscogsTrack[];
+  uri?: string;
+  videos?: unknown[];
+  year?: number;
+}
+
+export interface DiscogsMaster {
+  id: number;
+  title: string;
+  artists: DiscogsArtistSummary[];
+  data_quality?: string;
+  genres?: string[];
+  images?: DiscogsImage[];
+  lowest_price?: number;
+  main_release?: number;
+  main_release_url?: string;
+  most_recent_release?: number;
+  most_recent_release_url?: string;
+  num_for_sale?: number;
+  resource_url: string;
+  styles?: string[];
+  tracklist?: DiscogsTrack[];
+  uri?: string;
+  versions_url?: string;
+  videos?: unknown[];
+  year?: number;
+}
+
+export interface DiscogsArtist {
+  id: number;
+  name: string;
+  realname?: string;
+  profile?: string;
+  releases_url?: string;
+  resource_url: string;
+  uri?: string;
+  urls?: string[];
+  namevariations?: string[];
+  aliases?: DiscogsArtistSummary[];
+  groups?: DiscogsArtistSummary[];
+  members?: DiscogsArtistSummary[];
+  images?: DiscogsImage[];
+}
+
+export interface DiscogsLabel {
+  id: number;
+  name: string;
+  profile?: string;
+  contact_info?: string;
+  data_quality?: string;
+  parent_label?: DiscogsArtistSummary;
+  sublabels?: DiscogsArtistSummary[];
+  urls?: string[];
+  images?: DiscogsImage[];
+  releases_url?: string;
+  resource_url: string;
+  uri?: string;
+}
+
+export interface DiscogsVersionsParams extends DiscogsPaginationParams {
+  format?: string;
+  label?: string;
+  released?: string;
+  country?: string;
+  sort?: "released" | "title" | "format" | "label" | "catno" | "country";
+  sortOrder?: "asc" | "desc";
+}
+
+export type DiscogsVersionsResponse = DiscogsPaginatedResponse<DiscogsSearchResult>;
